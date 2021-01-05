88f3bcd2ebb4bbc40ad4e0f2f3fe0e0a252c6907
Sergio Rosa <scarlosjazz@gmail.com>
2021-01-05 17:05:00 -0500

    GET /orders

---
+++ backend/src/main/java/com/sergiorosa/dsdeliver/controllers/OrderController.java
@@ -0,0 +1,27 @@
+package com.sergiorosa.dsdeliver.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sergiorosa.dsdeliver.dto.OrderDTO;
+import com.sergiorosa.dsdeliver.services.OrderService;
+
+@RestController
+@RequestMapping(value = "/orders")
+public class OrderController {
+	
+	@Autowired
+	private OrderService service;
+	
+	@GetMapping
+	public ResponseEntity<List<OrderDTO>> findAll(){
+		List<OrderDTO> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+		}
+
+}
