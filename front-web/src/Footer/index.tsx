9c1c048b513e67e5ca3264a0769ad9a78a84212d
Sergio Rosa <scarlosjazz@gmail.com>
2021-01-07 05:25:14 -0500

    Add Footer

---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,22 @@
+import './styles.css';
+import { ReactComponent as GithubIcon } from './github.svg';
+import { ReactComponent as LinkedInIcon } from './linkedin.svg';
+
+function Footer() {
+  return (
+    <footer className="main-footer">
+      App developed during the second event DevSuperior Week
+      <div className="footer-icons">
+        <a href="https://github.com/srosajazz/dsdeliver-sds2" target="_new">
+          GithubIcon
+        </a>
+
+        <a href="https://www.linkedin.com/in/sergiorosa1/" target="_new">
+          LinkedInIcon
+        </a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,28 +1,32 @@
 import './styles.css';
 import { ReactComponent as MainImage } from './main.svg';
+import Footer from '../Footer';
 
 function Home() {
   return (
-    <div className="home-container">
-      <div className="home-content">
-        <div className="home-actions">
-          <h1 className="home-title">
-            Place your order <br /> we will deliver <br /> for you !!!
-          </h1>
-          <h3 className="home-subtitle">
-            Choose your order and in a few <br /> minutes we will deliver on
-            your door
-          </h3>
-          <a href="orders" className="home-btn-order">
-            ORDER
-          </a>
-        </div>
-        {/* SIDE-IMAGE */}
-        <div className="home-image">
-          <MainImage />
+    <>
+      <div className="home-container">
+        <div className="home-content">
+          <div className="home-actions">
+            <h1 className="home-title">
+              Place your order <br /> we will deliver <br /> for you !!!
+            </h1>
+            <h3 className="home-subtitle">
+              Choose your order and in a few <br /> minutes we will deliver on
+              your door
+            </h3>
+            <a href="orders" className="home-btn-order">
+              ORDER
+            </a>
+          </div>
+          {/* SIDE-IMAGE */}
+          <div className="home-image">
+            <MainImage />
+          </div>
         </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 }
 
